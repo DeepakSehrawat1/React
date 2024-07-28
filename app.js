40010327ce6b0e5b21912f53184e719bbfee8964
DeepakSehrawat1 <deepaksehrawat150@gmail.com>
2024-07-28 12:23:58 +0530

    function component

--- app.js
+++ app.js
@@ -1,19 +1,52 @@
-const heading1 = React.createElement(
-  "h1",
-  { style: { backgroundColor: "red" }, id: "toyo" },
-  "hello World"
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+/*const HeadingComponent = function () {
+  return (
+    <div>
+      <h3>uioii</h3>
+    </div>
+  );
+};
 
-const heading2 = React.createElement(
-  "h2",
-  { style: { backgroundColor: "green" }, id: "toyo2" },
-  "hello World2"
-);
+const head = <h1>pupo</h1>;
+const heading = (
+  <div>
+    <HeadingComponent />
+    {head}
+    <h1>okytioui</h1>
+    <h2>nokytoi</h2>
+  </div>
+);*/
 
-const divele = React.createElement(
-  "h3",
-  { style: { backgroundColor: "yellow" }, id: "toyo3" },
-  [heading1, heading2]
-);
+/*let heading1 = React.createElement("h1", {}, "logo");
+
+let header = React.createElement("Header", {}, [
+  React.createElement("h1", {}, "logo"),
+  React.createElement("h2", {}, "logo2"),
+  React.createElement("h3", {}, "logo3"),
+]);*/
+
+/*const header = (
+  <header>
+    <h1>logo</h1>
+    <h2>logo1</h2>
+    <h3>logo3</h3>
+  </header>
+);*/
+
+const HeaderComponent = () => {
+  return (
+    <header id="mainheader">
+      <img src="" alt="logo" id="first" />{" "}
+      <div id="searchsys">
+        <input type="text" placeholder="search" />
+        <button>search</button>
+      </div>{" "}
+      <a id="last">user</a>
+      <div className="clear"></div>
+    </header>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("container"));
-root.render(divele);
+root.render(<HeaderComponent />);
